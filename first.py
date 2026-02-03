09726bb0314279b96eef61665b021c6d37cb78a0
AbhaySharma1120 <abhaysharma4762@gmail.com>
2026-02-03 19:13:14 +0530

    Initial commit

---
+++ first.py
@@ -0,0 +1,51 @@
+# Question No.1
+
+x=int(input("Enter the first number- "))
+y=int(input("Enter the second number- "))
+sum=x+y
+difference = x-y
+product=x*y
+quo = x//y
+print(sum, difference, product, quo)
+
+# Question No.2
+a=15
+b=4
+floor = a//b
+remainder = a%b
+power = a**b
+print(floor, remainder, power)
+pen = 15
+notebook = 40
+total_cost = pen*3+notebook*2
+print(total_cost)
+
+# Question No.3
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+d=int(input("Enter fourth number: "))
+e=int(input("Enter five number: "))
+
+average = (a+b+c+d+e)/5;
+print(average)
+
+# Question No.4
+n = 2;
+square = n**2
+cube = n**3
+square_root = n**0.5
+print(square, cube, square_root)
+
+# Question No.5
+a = int(input("Enter total seconds: "))
+min = a//60
+sec = a%60
+c=min, sec;
+print(c)
+
+# Question No.6
+distance=120
+time=3
+average_speed=distance//time
+print(average_speed)    
