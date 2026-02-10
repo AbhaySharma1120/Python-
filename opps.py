f1ec67770360646b5766e98b173f92a273a71028
AbhaySharma1120 <abhaysharma4762@gmail.com>
2026-02-10 18:10:27 +0530

    Opps Concept

---
+++ opps.py
@@ -0,0 +1,63 @@
+# class Car:
+#     def __init__(self,brand,color):
+#         self.brand = brand
+#         self.color = color
+
+#     def start(self):
+#         print(f"{self.color} {self.brand} is starting...")
+
+# car1 = Car("Tesla", "Red")
+# car2 = Car("BMW", "Black")
+
+# car1.start()
+# car2.start()
+
+# class Employee:
+#     company = "TechCrop"
+
+#     def __init__(self,name,salary):
+#         self.name=name
+#         self.salary=salary
+
+# e1=Employee("John", 50000)
+# e2=Employee("Emma", 60000)
+
+# print(e1.company, e1.name, e1.salary)
+# print(e2.company, e2.name, e2.salary)
+
+# class Animal:
+#     def speak(self):
+#         print("Animals make sounds")
+
+# class Dog(Animal):
+#     def bark(self):
+#         print("Dog barks!")
+
+# d=Dog()
+# d.speak()
+# d.bark()
+
+# class Animal:
+#     def sound(self):
+#         print("Some generic sound")
+
+# class Cat(Animal):
+#     def sound(self):
+#         print("Meow!")
+
+# a=Animal()
+# c=Cat()
+# a.sound()
+# c.sound()
+
+# class Students:
+#     def __init__(self,name,roll_no,marks):
+#         self.name = name
+#         self.roll_no = roll_no
+#         self.marks = marks
+#     def Accept(self):
+#         print(f"{self.name} {self.roll_no} {self.marks}")
+
+# Students1 = Students("Abhay",123,23)
+# Students1.Accept()
+
