4f84e5621ad083c8eb8c47ed682a670c0bef1cbc
AbhaySharma1120 <abhaysharma4762@gmail.com>
2026-02-16 17:53:02 +0530

    new codes

---
+++ New.py
@@ -0,0 +1,51 @@
+#Create a dictionary of 5 countries and their capitals. Print it.
+countries = {
+    "India": "Delhi",
+    "Russia": "Moscow",
+    "USA": "Washington",
+    "Australia": "Canberra",
+    "Brazil": "Brasilia",
+}
+# print(countries)
+# print(countries["India"])
+# countries["Japan"] = "Tokiyo"
+# print(countries)
+# countries["USA"] = "New York"
+# print(countries)
+# del countries["Brazil"]
+# print(countries)
+
+#Create a dictionay of student and marks. Use .keys() to print all student names
+# students = {
+#     "Abhay" : 100,
+#     "Adarsh" : 100,
+#     "Abhishek" : 88,
+#     "Akash" : 70,
+#     "Daksh" : 78,
+# }
+
+# print("All students names: ")
+# for name in students.keys():
+#     print(name)
+
+# countries.pop("USA")
+# print(countries)
+
+# sentence = input("Enter a sentence: ")
+# words = sentence.split()
+# frequency= {}
+
+# for word in words:
+#     if word in frequency:
+#         frequency[word] +=1
+#     else:
+#         frequency[word] = 1
+# print(frequency)
+
+students = [
+    {"name": "Amit", "age":20},
+    {"name": "Neha","age":22},
+    {"name": "Rahul","age": 19}
+]
+
+#Create a list of 3 dictionaries, each containing student "name" and "age".
--- first.py
+++ first.py
@@ -187,9 +187,9 @@
 #     if(i%2==0):
 #         print(i)
 
-rows = 5
-for i in range(1, rows + 1):     
-    for j in range(1, i + 1):
-        print("* ", end="")                
-    print("")
+# rows = 5
+# for i in range(1, rows + 1):     
+#     for j in range(1, i + 1):
+#         print("* ", end="")                
+#     print("")
 
